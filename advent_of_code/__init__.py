a3e1aab2d7f521e3d9ae925e3b0085b82899e3a2
Bill <bill@thewallisfamily.co.uk>
2024-11-25 21:51:42 +0000

    ✨ feat: add a CLI

--- advent_of_code/__init__.py
+++ advent_of_code/__init__.py
@@ -1,3 +1,11 @@
 """
 Yearly packages to solve the Advent of Code problems.
 """
+
+from advent_of_code.solutions import print_solutions
+from advent_of_code.utils import create_files
+
+__all__ = [
+    "create_files",
+    "print_solutions",
+]
