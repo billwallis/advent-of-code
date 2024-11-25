a29d374f96dbc91a72d2d8bb3c930a6eeeea8f28
Bill <bill.wallis@sainsburys.co.uk>
2024-11-25 09:13:41 +0000

    🚨 refactor: bot updates

--- advent_of_code/year_2023/day_03/main.py
+++ advent_of_code/year_2023/day_03/main.py
@@ -1,6 +1,7 @@
 """
 Solution for day 3.
 """
+
 from __future__ import annotations
 
 import collections
@@ -214,7 +215,7 @@ class EngineSchematic:
 
         return image
 
-    def _part_numbers(self) -> Generator[PartNumber, None, None]:
+    def _part_numbers(self) -> Generator[PartNumber]:
         """
         Return the part numbers in the schematic.
         """
@@ -252,7 +253,7 @@ class EngineSchematic:
         # Can't cache a generator, so we cache the list instead
         return list(self._part_numbers())
 
-    def gear_ratios(self) -> Generator[int, None, None]:
+    def gear_ratios(self) -> Generator[int]:
         """
         Return the gear ratios in the schematic.
 
