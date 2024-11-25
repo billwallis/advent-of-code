424a4b9350d19543983746b9779aa0e19681e417
Bill <bill.wallis@sainsburys.co.uk>
2024-11-25 09:29:13 +0000

    ✨ feat: prep for 2024

--- advent_of_code/__main__.py
+++ advent_of_code/__main__.py
@@ -8,16 +8,17 @@ import advent_of_code.solutions
 import advent_of_code.utils
 
 
+# TODO: Add a CLI so that we don't need to comment code in and out
 def main() -> None:
     """
     Print the solutions.
     """
-    # advent_of_code.utils.create_files(year=2023, day=None)
+    # advent_of_code.utils.create_files(year=2024, day=1)
 
     advent_of_code.solutions.print_solutions(
         print_all=False,
-        year=2023,
-        # print_day=1,
+        year=2024,
+        print_day=1,
     )
 
 
---
+++ advent_of_code/year_2024/day_01/__init__.py
@@ -0,0 +1,7 @@
+"""
+Day 1:
+
+https://adventofcode.com/2024/day/1/input
+"""
+
+from advent_of_code.year_2024.day_01.main import solution
