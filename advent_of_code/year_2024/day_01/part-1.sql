3402c2ddf236468be909986ddbcc759241bea625
Bill <bill@thewallisfamily.co.uk>
2024-12-01 14:57:27 +0000

    ✨ feat: add SQL solution for day 1 (2024)

---
+++ advent_of_code/year_2024/day_01/part-1.sql
@@ -0,0 +1,27 @@
+with
+
+input(data) as (
+    select *
+    from read_csv(
+        -- 'advent_of_code/year_2024/day_01/sample.data',
+        'advent_of_code/year_2024/day_01/input.data',
+        -- 'https://adventofcode.com/2024/day/1/input',
+         header=false
+    )
+),
+
+ordered_input as (
+    select
+        data,
+        split(data, ' ')[1]::int as left_list,
+        split(data, ' ')[-1]::int as right_list,
+        row_number() over (order by left_list) as left_rn,
+        row_number() over (order by right_list) as right_rn,
+    from input
+)
+
+select sum(abs(left_list.left_list - right_list.right_list)) as solution
+from ordered_input as left_list
+    inner join ordered_input as right_list
+        on left_list.left_rn = right_list.right_rn
+;
